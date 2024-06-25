3f22198b09aeb4713a2c3f3480d9e1932e4b19b1
Shubham Jain <43323282+shubham1492@users.noreply.github.com>
2024-06-25 08:12:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+This repo is used to provide git information.
---
+++ src/DemoExample.java
@@ -0,0 +1,6 @@
+public class DemoExample {
+    public static void main(String[] args) {
+        System.out.println("Demo git");
+        System.out.println("hello");
+    }
+}
